de3c49e9d0ce47dbb3b3ae248fa65b5118c9acb9
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-02-28 15:08:36 -0300

    Finalizando a parte de notificações com o notifyhelper, com as limitações do windowns não existe uma forma de notificar porém em aparelhos celulares a notificação é exibida, criando a pasta images

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Agende seu Horario',
       debugShowCheckedModeBanner: false,
       theme: Themes.lightTheme,
       darkTheme: Themes.darkTheme,
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables
 
+import 'package:barberapp/ui/services/notification_service.dart';
 import 'package:barberapp/ui/services/theme_services.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +13,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +44,11 @@ class _HomePageState extends State<HomePage> {
       leading: GestureDetector(
         onTap: () {
           ThemeService().themeSwitch();
+          notifyHelper.displayNotification(
+            title: 'Mudança de Tema',
+            // body: Get.isDarkMode ?   "Usando o tema light" : "Usando o Tema Dark", //Troca de mensagens feita pois o getx está recebendo o pacote getx e não do _box do theme_services
+          );
+          notifyHelper.scheduledNotification();
         },
         child: Icon(
           Icons.nightlight_round,
