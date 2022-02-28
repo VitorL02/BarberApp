24ec106ab2c6c47399081556b0f3a6bf4b663e02
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-02-28 17:30:20 -0300

    Criando agora a dateBar que consta todas as datas disponiveis para agendar os horarios, utilizando a dependencia date_picker_timeline e o widget DatePicker

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,11 +1,13 @@
-// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables, unused_element, avoid_unnecessary_containers
 
 import 'package:barberapp/ui/services/notification_service.dart';
 import 'package:barberapp/ui/services/theme_services.dart';
 import 'package:barberapp/ui/theme.dart';
 import 'package:barberapp/ui/widgets/button.dart';
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,6 +18,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateTime _selectedDate = DateTime.now();
   var notifyHelper;
 
   @override
@@ -33,33 +36,67 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         // ignore: prefer_const_literals_to_create_immutables
         children: [
+          _addTaskBar(),
+          _addDateBar(),
+        ],
+      ),
+    );
+  }
+
+  _addDateBar() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 20),
+      child: DatePicker(
+        DateTime.now(),
+        height: 100,
+        width: 80,
+        initialSelectedDate: DateTime.now(),
+        selectionColor: primaryClr,
+        selectedTextColor: Colors.white,
+        onDateChange: (date) {
+          _selectedDate = date;
+        },
+        dateTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w600, color: Colors.grey),
+        ),
+        dayTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.w600, color: Colors.grey),
+        ),
+        monthTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+              fontSize: 14, fontWeight: FontWeight.w600, color: Colors.grey),
+        ),
+      ),
+    );
+  }
+
+  _addTaskBar() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
           Container(
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        DateFormat.yMMMMd().format(DateTime.now()),
-                        style: subHeadStyle,
-                      ),
-                      Text(
-                        'Hoje',
-                        style: headingStyle,
-                      ),
-                    ],
-                  ),
+                Text(
+                  DateFormat.yMMMMd().format(DateTime.now()),
+                  style: subHeadStyle,
                 ),
-                Button(
-                  label: "+ Novo Horário",
-                  onTap: () => null,
-                )
+                Text(
+                  'Hoje',
+                  style: headingStyle,
+                ),
               ],
             ),
-          )
+          ),
+          Button(
+            label: "+ Novo Horário",
+            onTap: () => null,
+          ),
         ],
       ),
     );
